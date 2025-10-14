5553ec9792a3f3ac1791920bba7b158e3467207d
rangaswamy9999 <damarangaswamy154@gmail.com>
2025-10-14 17:18:58 +0530

    vedio 14

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
+import React from "react";
 
+const data = [
+  {
+    title: "Avengers Endgame",
+    imgURL: "https://m.media-amazon.com/images/I/51oBxmV-dML._AC_SY445_.jpg",
+    description: "lorem 24 ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+  },
+  {
+    title: "Avatar",
+    imgURL: "https://m.media-amazon.com/images/I/51oBxmV-dML._AC_SY445_.jpg",
+    description: "lorem 24 ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+  },
+  {
+    title: "Titanic",
+    imgURL: "https://m.media-amazon.com/images/I/51oBxmV-dML._AC_SY445_.jpg",
+    description: "lorem 24 ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+  }
+];
+
+const Movie = ({ title, imgURL, description }) => {
+  return (
+    <div className="movie-card">
+      <img src={imgURL} alt={title} />
+      <h1>{title}</h1>
+      <p>{description}</p>
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <h1>Learing React</h1>
-    </div>
+    <>
+      {data.map((movie, index) => (
+        <Movie
+          key={index}
+          title={movie.title}
+          imgURL={movie.imgURL}
+          description={movie.description}
+        />
+      ))}
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import PrintName from './App';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
+setInterval(() => {
+  root.render(<PrintName />);
+}, 1000);
+//root.render(<PrintName />);
